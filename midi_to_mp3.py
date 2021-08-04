f355bd4b74ee3ba813fc605c5cdd8032c28a56d9
Garnet Grimm <garnetgrimm720@gmail.com>
2021-08-04 12:05:44 -0400

    init commit

---
+++ midi_to_mp3.py
@@ -0,0 +1,20 @@
+from midi2audio import FluidSynth
+import glob
+import subprocess
+import os
+from pathlib import Path
+
+fs = FluidSynth('sf2.sf2')
+
+inDir = "samples"
+outDir = "output"
+
+for f in glob.glob(inDir + '/**/*.mid', recursive=True):
+    filename = os.path.basename(f)
+    outPath = outDir + f[len(inDir):-len(filename)]
+    Path(outPath).mkdir(parents=True, exist_ok=True)
+    wavFile = outPath + " " + filename.replace(".mid",".wav")
+    mp3File = outPath + " " + filename.replace(".mid",".mp3")
+    fs.midi_to_audio(f, wavFile)
+    subprocess.run(["ffmpeg", "-y", "-i", wavFile, "-acodec", "libmp3lame", mp3File])
+    os.remove(wavFile)
